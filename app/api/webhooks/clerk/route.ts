550478eecbe9038d4cd4990eb970623f561ac2aa
Kiran Kanth Reddy <kanthkiran020@gmail.com>
2024-05-26 22:48:42 +0530

    commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -70,7 +70,6 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
-
     // Set public metadata
     if (newUser) {
       console.log('true')
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -36,6 +36,6 @@ const UserSchema = new Schema({
   },
 });
 
-const User = models?.User || model("users", UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
